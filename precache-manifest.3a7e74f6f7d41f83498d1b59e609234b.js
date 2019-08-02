766613b013d69e8a4d43d437c149ff9f8121b717
Robert Ramirez <metaknight757@gmail.com>
2019-08-02 15:04:23 -0500

    Updates

---
+++ precache-manifest.3a7e74f6f7d41f83498d1b59e609234b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf06e2a15383c794c5222c662bb8abc",
+    "url": "/Akira957/clickyGame/index.html"
+  },
+  {
+    "revision": "7a22795d1ee220fb5d73",
+    "url": "/Akira957/clickyGame/static/css/main.1c32eb83.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Akira957/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7a22795d1ee220fb5d73",
+    "url": "/Akira957/clickyGame/static/js/main.57e45d38.chunk.js"
+  },
+  {
+    "revision": "b8f3d26e3798cfa035f8",
+    "url": "/Akira957/clickyGame/static/js/runtime~main.ff85d230.js"
+  }
+]);
